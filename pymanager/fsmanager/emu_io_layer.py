10ffb2881da45d38250a43c7b96ec05861f164fb
orca-eaa5a <dlfguswn@sju.ac.kr>
2022-06-23 00:51:23 +0900

    new io-manager

--- pymanager/fsmanager/emu_io_layer.py
+++ pymanager/fsmanager/emu_io_layer.py
@@ -1,55 +1,89 @@
-from os.path import splitext as get_extension, basename
-from fs.memoryfs import MemoryFS, SubFS
+from io import UnsupportedOperation
+from typing import Dict
 from fs.errors import DirectoryExists, ResourceNotFound
-from io_mode import WinIOMode, PyIOMode
-from fs_emu_util import convert_winpath_to_emupath
-from pymanager.fsmanager import fs_emu_util
+import fs
+from fs.memoryfs import MemoryFS, SubFS
+from fs_emu_util import convert_winpath_to_emupath, emu_path_join
 
 class EmuIOLayer:
-    def __init__(self, fs:MemoryFS) -> None:
-        self.emu_fs:MemoryFS = fs
-        self.partitions={
-            #"volule_letter": SubFS
-        }
+    emu_fs = None
+    partitions = {
+        #"volule_letter": SubFS
+    }
+    def __init__(self) -> None:
+        EmuIOLayer.emu_fs = fs.open_fs('mem://')
+        self.add_volume("c:")
         pass
     
-    def add_volume(self, volume_letter:str, subFS:SubFS):
-        self.partitions[volume_letter] = subFS
+    def add_volume(self, volume_letter:str):
+        volume_letter = volume_letter.lower()
+        subfs = EmuIOLayer.emu_fs.makedir(volume_letter)
+        EmuIOLayer.partitions[volume_letter] = subfs
 
     def _get_volume(self, volume_letter:str) -> SubFS:
-        return self.partitions[volume_letter]
+        return EmuIOLayer.partitions[volume_letter]
 
     def _check_valid_voulme(self, volume_letter:str):
-        if volume_letter in self.partitions:
+        if volume_letter in EmuIOLayer.partitions:
             return True
         return False
 
-    def create_dir(self, path:str, dir_name:str, recursive:bool=False):
+    def file_existing(self, volume_name:str, path:str, file_name:str, ftype:str) -> bool:
         """_summary_
-            Create virtual directory at pyfilesystem.MemoryFS
-
+            Check specific file or directory is in pyfilesystem.MemoryFS
         Args:
+            volume_name(str): volume_name
             path (str): absolute path
+                e.g) volume_name -> c:
+                    path -> /test/test/test.txt
+            file_name (str): file name string which want to create
+            ftype (str): directory or file
+        """
+        file_name = file_name.lower()
+        if not self._check_valid_voulme(volume_name):
+            return False
+        o = convert_winpath_to_emupath(emu_path_join(volume_name, path))
+        abs_path = emu_path_join(o["vl"], o["ps"])
+        full_path = emu_path_join(abs_path, file_name)
+
+        if ftype == 'directory':
+            return EmuIOLayer.emu_fs.isdir(full_path)
+        elif ftype == 'file':
+            return EmuIOLayer.emu_fs.isfile(full_path)
+        else:
+            Exception('file_existing() invalid file type')
+        
+        pass
+
+    def create_file(self, volume_name:str, path:str, file_name:str, ftype:str, turncated=False, recursive:bool=False) -> Dict[bool, str, str, ]:
+        """_summary_
+            Create virtual file at pyfilesystem.MemoryFS
+        Args:
+            volume_name(str): volume_name
+            path (str): absolute path
+                e.g) volume_name -> c:
+                    path -> /test/test/test.txt
+            file_name (str): file name string which want to create
+            ftype (str): directory or file
             recursive (bool, optional): 
                 create directory with recursive option
                 if this option is setted, all directory related with path is created recursivly
         """
         ret = {
             'success': False,
-            'volume': '',
-            'path': '',
-            'object': None
+            'fp': ''
         }
-
-        o = convert_winpath_to_emupath(path)
-        if not self._check_valid_voulme(o["vl"]):
+        file_name = file_name.lower()
+        if not self._check_valid_voulme(volume_name):
             return ret
-        
-        volume = self._get_volume(o["vl"])
+        o = convert_winpath_to_emupath(emu_path_join(volume_name, path))
+        abs_path = emu_path_join(o["vl"], o["ps"])
+        full_path = emu_path_join(abs_path, file_name)
 
+        volume = self._get_volume(o["vl"])
+    
         if recursive:
             paths = o["ps"].split("/")
-            paths.append(dir_name)
             cur_path = ""
             for p in paths:
                 cur_path += p + "/"
@@ -57,219 +91,190 @@ class EmuIOLayer:
                     volume.makedir(cur_path)
                 except DirectoryExists:
                     continue
-        else:
-            volume.makedir(o["ps"]+"/"+dir_name)
         
+        if ftype == 'directory':
+            try:
+                EmuIOLayer.emu_fs.makedir(full_path)
+            except DirectoryExists:
+                pass
+        elif ftype == 'file':
+            EmuIOLayer.emu_fs.create(path=full_path, wipe=turncated)
+        else:
+            Exception('create_file() invalid file type')
+
         ret["success"] = True
-        ret["volume"] = o["vl"]
-        ret["path"] = o["ps"]
-        ret["object"] = volume.opendir(o["ps"] + "/" + dir_name)
+        ret["fp"] = full_path
 
         return ret
 
-    def create_file(self, path:str, filename:str, recursive:bool=False):
+    def open_file(self, volume_name:str, path:str, file_name:str, ftype:str, mode:str):
         """_summary_
-            Create virtual file at pyfilesystem.MemoryFS
-            Not related with CreateFile
-            This function only do file creation
+            Open file object which in pyfilesystem.MemoryFS
         Args:
+            volume_name(str): volume_name
             path (str): absolute path
-            working_dir (str): current path of emulation process
-            recursive (bool, optional): 
-                create directory with recursive option
-                if this option is setted, all directory related with path is created recursivly
+                e.g) volume_name -> c:
+                    path -> /test/test/test.txt
+            file_name (str): file name string which want to create
+            ftype (str): directory or file
+            mode (str): 
+                python-like file open mode
+                e.g) wb
         """
         ret = {
-            'success': True,
-            'path': '',
-            'filename': ''
+            "success": False,
+            "fp": '',
+            "ftype": '',
+            "obj": None
         }
-        if recursive:
-            dir_name = basename(path)
-            self.create_dir(path[:-1*len(dir_name)], dir_name, recursive)
+        file_name = file_name.lower()
+
+        pinfo = convert_winpath_to_emupath(emu_path_join(volume_name, path, file_name))
+        full_path = emu_path_join(pinfo["vl"], pinfo["ps"], file_name)
+
+        ret["fp"] = full_path
+        ret["mode"] = mode
+        ret["ftype"] = ftype
+
+        obj = None
         try:
-            subfs = self.emu_fs.opendir(path)
-            subfs.touch(filename)
-            ret["path"] = path
-            ret["filename"] = filename
+            if ftype == 'directory':
+                obj = EmuIOLayer.emu_fs.opendir(full_path)
+            elif ftype == 'file':
+                obj = EmuIOLayer.emu_fs.open(full_path, mode=mode)
+            else:
+                Exception('open_file() invalid file type')
         except ResourceNotFound:
-            ret["success"] = False
+            return ret
+
+        ret["success"] = True
+        ret["obj"] = obj
 
         return ret
-        
 
-
-class FileIOManager:
-    def __init__(self, fs:MemoryFS()):
-        self.file_system:MemoryFS()=fs
-        self.py_io_mode=PyIOMode.mode
-        self.win_io_mode=WinIOMode()
-        self.file_handle_manager=EmFileManager()
-        self.working_dir = "c:/users/orca/desktop"
-
-    def make_virtual_dir(self, virtual_dir_path, force=False):
-        if "/" in virtual_dir_path:
-            if force:
-                _path = ""
-                paths = virtual_dir_path.split("/")
-                for _dir in paths:
-                    _path += _dir
-                    if not self.file_system.exists(_path):
-                        self.file_system.makedir(_path)
-                    _path+="/"
-            else:
-                if not self.file_system.exists(virtual_dir_path):
-                    self.file_system.makedir(virtual_dir_path)
-            return True
-        else:
-            return False
-         
-    def write_virtual_file(self, virtual_file_full_path, _bytes, force=False):
-        # if force, create all directory, after then write the file
-        virtual_file_full_path = self.convert_path_unix_fmt(virtual_file_full_path)
-        if force:
-            virtual_dir_path = "/".join(virtual_file_full_path.split("/")[0:-1])
-            self.make_virtual_dir(virtual_dir_path, force=force)
-        self.file_system.writebytes(virtual_file_full_path, _bytes)
-        pass
-
-    def read_virtual_file(self, file_full_path):
-        pass
-    def delete_virtual_file(self, file_full_path):
-        pass
-
-    def convert_path_unix_fmt(self, file_path:str):
-        return file_path.replace("\\", "/").lower()
-
-    def set_current_dir(self, d):
-        d = self.convert_path_unix_fmt(d)
-        self.working_dir = d
-
-    # Change the Windows IO mode to Python IO Mode
-    def convert_io_mode(self, f_name, desired_access, c_dispotion)->str:
-        f_name = self.convert_path_unix_fmt(file_path=f_name)
-        mode=""
-        # Over Privileged Policy
-        if self.is_only_fname(f_name):
-            f_name = self.working_dir + "/" + f_name
-        if self.file_system.exists(f_name): # If file alread exist
-            if c_dispotion in [self.win_io_mode.c_disposition.OPEN_ALWAYS,
-                            self.win_io_mode.c_disposition.OPEN_EXISTING,
-                            self.win_io_mode.c_disposition.CREATE_NEW]: # R/W
-                if desired_access & self.win_io_mode.d_access.FILE_APPEND_DATA:
-                    mode=self.py_io_mode["aa"]
-                else:
-                    mode=self.py_io_mode["rw"]
-            elif c_dispotion == self.win_io_mode.c_disposition.TRUNCATE_EXISTING: # Create New and R/W
-                mode=self.py_io_mode["wr"]
-            else: # CREATE_ALWAYS
-                if desired_access & self.win_io_mode.d_access.FILE_APPEND_DATA:
-                    mode=self.py_io_mode["aa"]
-                else:
-                    mode=self.py_io_mode["wr"]
-        else:
-            if c_dispotion == self.win_io_mode.c_disposition.OPEN_EXISTING:
-                raise Exception("File is not exist")
-            elif c_dispotion in [self.win_io_mode.c_disposition.CREATE_NEW, 
-                                self.win_io_mode.c_disposition.CREATE_ALWAYS,
-                                self.win_io_mode.c_disposition.OPEN_ALWAYS]:
-                mode=self.py_io_mode["wr"]
-            else: # TRUNCATE_EXISTING
-                raise Exception("File is not exist")
-        
-        return mode
-    
-    def is_only_fname(self,file_path):
-        file_path = self.convert_path_unix_fmt(file_path=file_path)
-        if "/" not in file_path:
-            return True
-        else:
-            return False
-
-    def create_file(self, file_path, mode=None)->EmFile:
-        if not mode:
-            mode=self.py_io_mode["ro"] # Read Only
-        file_path = self.convert_path_unix_fmt(file_path)
-        if "./" in file_path or "../" in file_path:
-            file_path = self.get_relative_path(file_path)
-        else:
-            if self.is_only_fname(file_path=file_path):
-                file_path = self.working_dir + "/" + file_path
+    def write_data(self, obj, data:bytes, offset:int=-1):
+        """_summary_
+            Write the binary data at file object of pyfilesystem.MemoryFS
+        Args:
+            obj(_io.TextIOWrapper): MemoryFS file object
+            data (bytes): data to write
+            offset (int): 
+                offset to write data.
+                if offset == -1,
+                    write offset is the location where file pointer located
+                else,
+                    write data at specific offset.
+                    after write data, check current and recover original file pointer offset or not
+        """
+        ret = {
+            "success": False,
+            "fp": '',
+            "ws": '', # written size
+            "obj": None
+        }
+        old_offset = obj.tell()
+        if offset != -1:
+            # write data at specific offset
+            obj.seek(0)
+            obj.seek(offset)
         try:
-            fp = self.file_system.open(file_path, mode)
-            file_handle = self.file_handle_manager.create_file_handle(fp)
-        except FileNotFoundError:
-            file_handle = windef.INVALID_HANDLE_VALUE
+            obj.write(data)
+        except OSError:
+            return ret
         
-        return file_handle
+        if offset != -1:
+            cur_offset = obj.tell()
+            if cur_offset < old_offset:
+                obj.seek(old_offset)
+        
+        ret["success"] = True
+        ret["fp"] = obj.name[1:]
+        ret["ws"] = len(data)
+        ret["obj"] = obj
 
-    def write_file(self, file_handle, data):
-        file_obj = obj_manager.ObjectManager.get_obj_by_handle(file_handle)
-        file_obj.fp.write(data)
-        file_obj.fp.flush()
-
-        return len(data)
-
-    def read_file(self, file_handle, read_bytes=0xFFFFFFFF)->bytes:
-        file_obj = obj_manager.ObjectManager.get_obj_by_handle(file_handle)
-
-        if read_bytes == 0xFFFFFFFF:
-            buf = file_obj.fp.read() # Read ALL
-        else:
-            buf = file_obj.fp.read(read_bytes)
-
-        return buf
-
-    def close_file(self, file_handle):
-        self.file_handle_manager.close_file_handle(file_handle)
-
-    def create_file_mapping(self, file_handle, map_max, protect, name)->EmMMFile:
-        if file_handle == 0xFFFFFFFF: # Invalid File Handle
-            file_handle = self.create_file("C:/pagefile.sys", "wb+")
-        mmf_handle = self.file_handle_manager.create_file_mapping_handle(file_handle, map_max, protect, name)
-
-        return mmf_handle
-
-    def set_map_object(self, file_handle, offset, map_region)->EmMMFile:
-        mmf_obj:EmMMFile = obj_manager.ObjectManager.get_obj_by_handle(file_handle)
-        self.set_file_pointer(file_handle, offset)
-        mmf_obj.set_view(map_region)
-
-        return mmf_obj
-
-    def get_file_pointer(self, file_handle):
-        file_obj = obj_manager.ObjectManager.get_obj_by_handle(file_handle)
-        return file_obj.fp.ftell()
+        return ret
     
-    def set_file_pointer(self, file_handle, offset):
-        file_obj = obj_manager.ObjectManager.get_obj_by_handle(file_handle)
-        return file_obj.fp.seek(offset)
-
-    def get_current_directory(self):
-        return self.working_dir
-    
-    def set_current_directory(self, dir_path):
-        dir_path = self.convert_path_unix_fmt(dir_path)
-        if self.file_system.exists(dir_path):
-            self.working_dir = dir_path
-
-            return self.working_dir
+    def read_data(self, obj, offset=-1, read_size=-1):
+        """_summary_
+            Read the binary data from file object of pyfilesystem.MemoryFS
+        Args:
+            obj(_io.TextIOWrapper): MemoryFS file object
+            read_size : requested size to read. when read_size is -1, read all data from current file pointer location
+            offset (int): 
+                offset to read data from file.
+                if offset == -1,
+                    read offset is the location where file pointer located
+                else,
+                    read data from specific offset.
+                    after read data, check current and recover original file pointer offset or not
+        """
+        ret = {
+            "success": False,
+            "fp": '',
+            "rs": 0, # read size
+            'data': b'',
+            "obj": None
+        }
+        ret["fp"] = obj.name[1:]
+        
+        old_offset = obj.tell()
+        b = b''
+        if offset == -1:
+            obj.seek(0)
         else:
-            raise Exception("Directory is not exist")
-            return ""
-
-    def get_relative_path(self, file_path):
-        paths = file_path.split("/")
-        if paths[0] == ".":
-            return "/".join([self.working_dir] + paths[1:])
-
-        relative_dir = self.working_dir.split("/")
-
-        for path in paths:
-            if path == "..":
-                relative_dir = relative_dir[:-1]
+            obj.seek(offset)
+        try:
+            if read_size == -1:
+                b = obj.read()
             else:
-                relative_dir.append(path)
+                b = obj.read(read_size)
+        except UnsupportedOperation:
+            ret["ws"] = -1
+            ret["data"] = b''
+            return ret
         
+        obj.seek(old_offset)
 
-        return "/".join(relative_dir)
+        ret["success"] = True
+        ret["rs"] = len(b)
+        ret["data"] = b
+        ret["obj"] = obj
+
+    def delete_file(self, volume_name:str, path:str, file_name:str, ftype:str):
+        ret = {
+            "success": False,
+            "fp": '',
+        }
+
+        o = convert_winpath_to_emupath(emu_path_join(volume_name, path))
+        abs_path = emu_path_join(o["vl"], o["ps"])
+        full_path = emu_path_join(abs_path, file_name)
+
+        try:
+            if ftype == 'directory':
+                EmuIOLayer.emu_fs.removedir(full_path)
+            elif ftype == 'file':
+                EmuIOLayer.emu_fs.remove(full_path)
+            else:
+                Exception('delete_file() invalid file type')
+        except ResourceNotFound:
+            return ret
+        
+        ret["fp"] = full_path
+        
+        return ret
+
+
+    # def create_file_mapping(self, file_handle, map_max, protect, name)->EmMMFile:
+    #     if file_handle == 0xFFFFFFFF: # Invalid File Handle
+    #         file_handle = self.create_file("C:/pagefile.sys", "wb+")
+    #     mmf_handle = self.file_handle_manager.create_file_mapping_handle(file_handle, map_max, protect, name)
+
+    #     return mmf_handle
+
+    # def set_map_object(self, file_handle, offset, map_region)->EmMMFile:
+    #     mmf_obj:EmMMFile = obj_manager.ObjectManager.get_obj_by_handle(file_handle)
+    #     self.set_file_pointer(file_handle, offset)
+    #     mmf_obj.set_view(map_region)
+
+    #     return mmf_obj
--- pymanager/fsmanager/fs_emu_util.py
+++ pymanager/fsmanager/fs_emu_util.py
@@ -1,5 +1,6 @@
-from typing import Tuple
+from typing import Tuple, Dict
 import windefs as nt
+from os.path import basename
 
 def is_winpath(path:str) -> bool:
     if "\\" in path:
@@ -17,6 +18,78 @@ def is_abspath(path:str) -> bool:
     if ":" in path:
         return True
     return False
+
+def emu_path_join(path1, *args):
+    for _path in args:
+        if _path[0] == "/":
+            path1+=_path
+        else:
+            path1+= "/" + _path
+
+    return path1
+
+def splitdrive(p):
+    # modify splitdrive of https://github.com/python/cpython/blob/3.10/Lib/ntpath.py
+    """Split a pathname into drive/UNC sharepoint and relative path specifiers.
+    Returns a 2-tuple (drive_or_unc, path); either part may be empty.
+    If you assign
+        result = splitdrive(p)
+    It is always true that:
+        result[0] + result[1] == p
+    If the path contained a drive letter, drive_or_unc will contain everything
+    up to and including the colon.  e.g. splitdrive("c:/dir") returns ("c:", "/dir")
+    If the path contained a UNC path, the drive_or_unc will contain the host name
+    and share up to but not including the fourth directory separator character.
+    e.g. splitdrive("//host/computer/dir") returns ("//host/computer", "/dir")
+    Paths cannot contain both a drive letter and a UNC path.
+    """
+    if len(p) >= 2:
+        if isinstance(p, bytes):
+            sep = b'/'
+            altsep = b'\\'
+            colon = b':'
+        else:
+            sep = '/'
+            altsep = '\\'
+            colon = ':'
+        normp = p.replace(altsep, sep)
+        if (normp[0:2] == sep*2) and (normp[2:3] != sep):
+            # is a UNC path:
+            # vvvvvvvvvvvvvvvvvvvv drive letter or UNC path
+            # \\machine\mountpoint\directory\etc\...
+            #           directory ^^^^^^^^^^^^^^^
+            index = normp.find(sep, 2)
+            if index == -1:
+                return p[:0], p
+            index2 = normp.find(sep, index + 1)
+            # a UNC path can't have two slashes in a row
+            # (after the initial two)
+            if index2 == index + 1:
+                return p[:0], p
+            if index2 == -1:
+                index2 = len(p)
+            return p[:index2], p[index2:]
+        if normp[1:2] == colon:
+            return p[:2], p[2:]
+    return p[:0], p
+
+def parse_file_fullpath(abs_full_path:str) -> Tuple[str, str, str]:
+    """_summary_
+        parse input string to (volume_letter, path, file_name)
+    Args:
+        abs_full_path (str): string which indicate full path of file
+
+    Returns:
+        Tuple[str, str, str]: (volume_letter, path, file_name)
+    """
+
+    volume_letter, path = splitdrive(abs_full_path)
+    if not volume_letter:
+        return ('', '', '')
+    base_name = basename(path)
+    path = path[:int(-1*len(base_name))]
+
+    return (volume_letter, path, base_name)
 
 def get_parent_pathstring(abs_path:str) -> str:
     """
@@ -101,7 +174,7 @@ def convert_winpath_to_emupath(path:str, working_dir:str='') -> Tuple[str,str]:
         "ps": path_string
     }
 
-def convert_win_to_emu_iomode(dwDesiredAccess, dwCreationDisposition, dwFlagsAndAttributes) -> str:
+def convert_win_to_emu_iomode(dwDesiredAccess, dwCreationDisposition, dwFlagsAndAttributes) -> Dict[str, bool, bool, str]:
     """_summary_
         During emulation, emulator has most privilege policy
         Therefore, emulator give almost full privilege to emulating process
@@ -114,18 +187,29 @@ def convert_win_to_emu_iomode(dwDesiredAccess, dwCreationDisposition, dwFlagsAnd
         dwFlagsAndAttributes (_type_): _description_
 
     Returns:
-        str: python io mode
+        dictionary: python io mode
+            "ty" : open type (file or directory)
+            "nc" : create new file
+            "fc" : create file new file wether file is already existing or not
+            "mode" : python-like io mode
     """
     def is_directory_open(dwFlagsAndAttributes):
         if nt.FileAttribute.FILE_FLAG_BACKUP_SEMANTICS | dwFlagsAndAttributes:
             return True
         return False
 
-    def is_exist_only(dwCreationDisposition):
+    def is_create_new(dwCreationDisposition):
         if (dwCreationDisposition | nt.CreationDisposition.CREATE_ALWAYS) or \
+            (dwCreationDisposition | nt.CreationDisposition.CREATE_NEW) or \
             (dwCreationDisposition | nt.CreationDisposition.OPEN_ALWAYS):
             return False
         return True
+
+    def is_create_force(dwCreationDisposition):
+        if (dwCreationDisposition | nt.CreationDisposition.CREATE_ALWAYS) or \
+            (dwCreationDisposition | nt.CreationDisposition.OPEN_ALWAYS):
+            return True
+        return False
 
     def convert_io_mode_to_py(dwDesiredAccess, dwFlagsAndAttributes, is_overwrite):
         if (dwFlagsAndAttributes | nt.FileAttribute.FILE_ATTRIBUTE_READONLY):
@@ -151,12 +235,14 @@ def convert_win_to_emu_iomode(dwDesiredAccess, dwCreationDisposition, dwFlagsAnd
         
 
     open_type = 'directory' if is_directory_open(dwFlagsAndAttributes) else 'file'
-    failed_on_not_exist = True if is_exist_only(dwCreationDisposition) else False
+    new_create = True if is_create_new(dwCreationDisposition) else False
+    create_force = True if is_create_force(dwCreationDisposition) else False
     is_overwrite = True if (dwCreationDisposition | nt.CreationDisposition.CREATE_ALWAYS) else False
     py_io_mode = convert_io_mode_to_py(dwDesiredAccess, dwFlagsAndAttributes, is_overwrite)
 
     return {
         'ty': open_type, # open type
-        'eo': failed_on_not_exist, # exist only
+        'nc': new_create, # create new file
+        'cf': create_force, # create file forcefully
         'mode': py_io_mode
     }
